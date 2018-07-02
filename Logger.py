d3fb09a55519111157a70d5c59f3f7e47a2b8007
OhMesch <mze5111@gmail.com>
2018-07-01 22:31:10 -0400

    Inital commit

---
+++ Logger.py
@@ -0,0 +1,33 @@
+import os
+import time
+from datetime import datetime, timedelta
+
+class Logger:
+    def __init__(self):
+        self.initTime = datetime.now()
+        self.logsFolder = os.path.join(os.getcwd(), 'Logs')
+        self.logPath = os.path.join(self.logsFolder,self.initTime.strftime('%m-%d_%H:%M:%S.txt'))
+
+        self.createLogFolderIfDoesntExist()
+
+    def createLogFolderIfDoesntExist(self):
+        logsFolder = os.path.join(os.getcwd(), './Logs')
+        if(not os.path.isdir(logsFolder)):
+            os.makedirs(logsFolder)
+
+    def getLogPath(self):
+        return(self.logPath)
+
+    def printLn(self, logEntry):
+        logPath = self.getLogPath()
+        LINE_PREFEX = self.getLinePrefix()
+        logFile = open(logPath, 'a+')
+        logFile.write(LINE_PREFEX + logEntry+'\n')
+        logFile.close()
+
+    def getLinePrefix(self):
+        runTime = datetime.now() - self.initTime
+        runTimeH = runTime.seconds // 3600
+        runTimeM = runTime.seconds // 60
+        runTimeS = runTime.seconds % 3600
+        return('[%02d:%02d:%02d] ' % (runTimeH, runTimeM, runTimeS))
