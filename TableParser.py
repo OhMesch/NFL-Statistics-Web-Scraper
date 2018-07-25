32ef7cb0036d59d7c0aa738268478ced5ecd3114
OhMesch <mze5111@gmail.com>
2018-07-24 21:57:08 -0400

    Added functionality to parse for table title, column names and values

--- TableParser.py
+++ TableParser.py
@@ -7,19 +7,67 @@ class TableParser:
 	def __init__(self, table):
 		self.table = table
 		self.tableTitle = None
-		self.tableRowNames = None
+		self.tableColumnNames = []
+		self.tableRowValues = None
 
 	def parseTable(self):
-		self.setTableTitleFromDiv()
-		self.setRowNames()
+		self.autoSetTableTitle()
+		self.autoSetColumnNames()
+		self.autoFillRows()
+		self.printTableTitle()
+		self.printRowNames()
+		self.printTable()
 
-	def setTableTitleFromDiv(self):
-		self.tableTitle = self.table.find('div').text
-		print("Title is: " + self.tableTitle)
+	def autoSetTableTitle(self):
+		self.tableTitle = self.table.find('div').text.strip()
 
-	def setRowNames(self):
-		# print("Row names are:")
-		a = self.table.find_all('td')
-		print(a)
-		for c in a:
-			print(c.text)
+	def autoSetColumnNames(self):
+		tdTags = self.table.find_all('td')
+		currLine = 0
+		currText = tdTags[currLine].text.strip()
+
+		while currLine < len(tdTags) and currText != "Year":
+			currLine += 1
+			currText = tdTags[currLine].text.strip()
+
+		while currLine < len(tdTags) and not currText.isdigit(): #First entry is always year
+			if currText: 
+				self.tableColumnNames.append(currText)
+			currLine += 1
+			currText = tdTags[currLine].text.strip()
+
+	def autoFillRows(self):
+		numColumns = len(self.tableColumnNames)
+		self.tableRowValues = []
+		tdTags = self.table.find_all('td')
+		currLine = 1
+		currText = tdTags[currLine].text.strip()
+		while currLine < len(tdTags) and not currText.isdigit():
+			currLine += 1
+			currText = tdTags[currLine].text.strip()
+
+		dataCounter = 0
+		currRow = []
+		while currLine < len(tdTags) and currText != "TOTAL":
+			if currText:
+				if dataCounter < 13:
+					currRow.append(currText)
+					dataCounter += 1
+				else:
+					self.tableRowValues.append(currRow)
+					currRow = []
+					dataCounter = 0
+				
+			currLine += 1
+			currText = tdTags[currLine].text.strip()
+
+	def printTableTitle(self):
+		print(self.tableTitle)
+
+	def printRowNames(self):
+		print(self.tableColumnNames)
+
+	def printTable(self):
+		for row in self.tableRowValues:
+			print(row)
+		print('\n')
