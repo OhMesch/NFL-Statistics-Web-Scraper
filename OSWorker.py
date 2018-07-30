218180ecfae7aadcad3ba5e0e084f0a0f948612e
OhMesch <mze5111@gmail.com>
2018-07-29 21:48:08 -0400

    Improved functionality

--- OSWorker.py
+++ OSWorker.py
@@ -1,24 +1,35 @@
 import os
-from Logger import Logger
 
 class OSWorker:
 	#Should be using os.getpath...?
 	#If it is all static how will I pass in logger object
 	#If it is nonstatic, we can give it a reference of where to write
+	#Leave static and give CSVWriter access to logger
+	#What happens if somebody passes in a full file path?
+
 	def appendToFile(filePath, contentToAdd):
-		file = open(filePath, "a")
+		fullFilePath = os.path.join(os.getcwd(),filePath)
+		file = open(fullFilePath, "a")
 		file.write(contentToAdd)
 		file.close()
 
 	def createFile(filePath):
-		file = open(filePath,"w+")
+		fullFilePath = os.path.join(os.getcwd(),filePath)
+		file = open(fullFilePath,"w+")
 		file.close()
 
 	def createFolder(folderPath):
-		os.mkdirs(folderPath)
+		fullFolderPath = os.path.join(os.getcwd(),folderPath)
+		os.makedirs(fullFolderPath)
 
 	def deleteFile(filePath):
-		os.rmdir(filePath)
+		fullFilePath = os.path.join(os.getcwd(),filePath)
+		os.remove(fullFilePath)
+
+	def deleteFolder(filePath):
+		fullFilePath = os.path.join(os.getcwd(),filePath)
+		os.rmdir(fullFilePath)
 
 	def isExistingPath(path):
-		return(os.path.exists(path))
+		fullFilePath = os.path.join(os.getcwd(),path)
+		return(os.path.exists(fullFilePath))
