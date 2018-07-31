af581d3fbf761541a55b65411ae831781736c66d
OhMesch <mze5111@gmail.com>
2018-07-30 20:25:41 -0400

    Cleaned up

--- nflScraper.py
+++ nflScraper.py
@@ -4,37 +4,24 @@ from bs4 import BeautifulSoup
 from Logger import Logger
 
 class nflScraper:
-	def __init__(self):
-		self.visitsCount = 0
-		self.visitsLimit = 10
+	def __init__(self, loggerObj):
 		self.playerLinks = []
 		self.BASE_SITE = "http://www.nfl.com"
 		self.seenUrls = set()
-		self.logger = Logger()
-
-	def updatevisitsCount(self):
-		self.visitsCount += 1
-
-	def visitsCountIsAcceptable(self):
-		return self.visitsCount <= self.visitsLimit
+		self.logger = loggerObj
 
 	def scrapeUrlForLinks(self, url):
-		self.updatevisitsCount()
-		if(self.visitsCountIsAcceptable()):
-			try:
-				htmlSoup = self.getHTMLFromURL(url)
-			except requests.exceptions.RequestException as err:
-				self.logger.printLn("Unable to reach {}:\n{}\n".format(url,err))
-			else:
-				#is additionalLinks used?
-				#are we only looking at first page?
-				additionLinks = self.getLinksFromHTML(htmlSoup)
+		try:
+			htmlSoup = self.getHTMLFromURL(url)
+		except requests.exceptions.RequestException as err:
+			self.logger.printLn("Unable to reach {}:\n{}\n".format(url,err))
+		else:
+			self.getLinksFromHTML(htmlSoup)
 
 	def getHTMLFromURL(self, url):
 		code = requests.get(url)
 		htmlInPlainText = code.text
 		htmlSoup = BeautifulSoup(htmlInPlainText, "html.parser")
-		# print(htmlSoup.title.string)
 		return(htmlSoup)
 
 	def getLinksFromHTML(self, html):
@@ -50,7 +37,8 @@ class nflScraper:
 		if first8Char == "/player/":
 			self.playerLinks.append(link)
 		elif first8Char == "/players":
-			self.scrapeUrlForLinks(self.BASE_SITE+link)
+			nextPage = self.BASE_SITE+link
+			self.scrapeUrlForLinks(nextPage)
 		else:
 			self.logger.printLn("Unexpected link detected:\n%s\n" % link)
 
