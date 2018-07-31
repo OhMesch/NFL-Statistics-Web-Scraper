7cf44d2d6d6b8f5c5bc699bc8fb09dcbc1134f14
OhMesch <mze5111@gmail.com>
2018-07-30 21:26:20 -0400

    Minor changes to keep consistant with APIs

--- drivers/driverGetPlayers.py
+++ drivers/driverGetPlayers.py
@@ -1,6 +1,8 @@
 from nflScraper import nflScraper
+from Logger import Logger
 
-driver = nflScraper()
+logger = Logger()
+driver = nflScraper(logger)
 for i in range(97,97+26):
 	driver.scrapeUrlForLinks("http://www.nfl.com/players/search?category=lastName&playerType=current&d-447263-p=1&filter=%s" %chr(i))
-print(driver.getPlayerLinkList())
+print(driver.getPlayerLinks())
